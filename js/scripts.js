5bbe4d2edc4111c1407fb4278d8bd919752cb421
Guilherme Cabral <guilhermeoccorona@gmail.com>
2020-12-19 12:08:44 -0300

    Inclusão da alteração da cor do background da página

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,19 @@ function changeImage(element, newImg) {
     element.src = newImg
     element.onmouseout = () => element.src = olderImg
 }
+
+function changeBackground(newBackground) {
+    const body = document.querySelector('body')
+    body.classList.remove(Object.values(body.classList).filter(className => className.indexOf('background') != -1))
+    body.classList.add(newBackground)
+
+    if(newBackground === 'background-white') {
+        document.getElementById('logo').src = '../img/logo-blue.png'
+    } else {
+        document.getElementById('logo').src = '../img/logo-white.png'
+    }
+}
+
+function changeBackgroundImage(imageBackground) {
+    
+}
