f2ee56e5076025bda1f0e40e93d10c9d46cdb4d9
Guilherme Cabral <guilhermeoccorona@gmail.com>
2020-12-19 11:37:44 -0300

    Adicionado troca da imagem do cursor ao passar sobre os ícones da navbar

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 function changeImage(element, newImg) {
     const olderImg = element.src
+    element.style.cursor = 'pointer'
     element.src = newImg
     element.onmouseout = () => element.src = olderImg
 }
